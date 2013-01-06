31a63af31c11e483996964a8d983915a65bcacc1
dotcom900825 <dotcom900825@gmail.com>
2013-01-06 16:56:45 +0800

    weibo bug

--- pinry/weibo_oauth/views.py
+++ pinry/weibo_oauth/views.py
@@ -4,6 +4,11 @@ from django.template import RequestContext
 from weibo import APIClient
 from django.shortcuts import render_to_response
 from django.conf import settings
+from models import Profile
+from accounts.models import MyProfile
+from userena.models import UserenaSignup
+from userena import settings as userena_settings
+
 def weibo_login(request):
 	client = APIClient(app_key=settings.WEIBO_APP_KEY, app_secret=settings.WEIBO_APP_SECRET, redirect_uri=settings.WEIBO_REDIRECT)
 	url = client.get_authorize_url()
@@ -17,4 +22,31 @@ def register(request):
 		access_token = r.access_token
 		expires_in = r.expires_in
 		client.set_access_token(access_token,expires_in)
-		return HttpResponse(client.statuses.user_timeline.get())		
+		username = client.users.name
+		uid = client.users.id
+		avatar = client.users.profile_img_url
+		if UserenaSignup.objects.filter(user__username__iexact=uid):
+            pass
+        else:
+            new_user = UserenaSignup.objects.create_user(uid,
+                                                     username,
+                                                     '',
+                                                     access_token,
+                                                     not userena_settings.USERENA_ACTIVATION_REQUIRED,
+                                                     userena_settings.USERENA_ACTIVATION_REQUIRED)
+
+     #   user.set_password(access_token)
+     #   user.save()
+
+            #profile, profile_created = Profile.objects.get_or_create(user=new_user, name=name, avatar=avatar)
+            #profile.access_token = access_token
+            #profile.save()
+
+            myProfile = MyProfile.objects.get(user=new_user)
+            myProfile.socialImageUrl = avatar
+            myProfile.save()
+        # Authenticate the user and log them in using Django's pre-built
+        # functions for these things.
+        user = authenticate(username=uid, password=access_token)
+        login(request, user)
+        return HttpResponseRedirect('/')
