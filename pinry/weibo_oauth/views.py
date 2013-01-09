9034e16db5f8f4331a147fd2e655bdf61e0095d3
dotcom900825 <dotcom900825@gmail.com>
2013-01-09 16:59:28 +0800

    weibo fixed

--- pinry/weibo_oauth/views.py
+++ pinry/weibo_oauth/views.py
@@ -7,7 +7,7 @@ from django.conf import settings
 from accounts.models import MyProfile
 from userena.models import UserenaSignup
 from userena import settings as userena_settings
-
+from django.contrib.auth import authenticate,login
 def weibo_login(request):
 	client = APIClient(app_key=settings.WEIBO_APP_KEY, app_secret=settings.WEIBO_APP_SECRET, redirect_uri=settings.WEIBO_REDIRECT)
 	url = client.get_authorize_url()
@@ -20,8 +20,25 @@ def register(request):
 		r = client.request_access_token(code)
 		access_token = r.access_token
 		expires_in = r.expires_in
+		uid = r.uid
 		client.set_access_token(access_token,expires_in)
-		username = client.users.name
-		uid = client.users.id
-		avatar = client.users.profile_img_url
-        return HttpResponseRedirect(username)
+		user = client.users.show.get(uid=uid)
+		username = user.name
+		avatar = user.profile_image_url
+		if UserenaSignup.objects.filter(user__username__iexact=username):
+			pass
+        	else:
+            		new_user = UserenaSignup.objects.create_user(username,
+                                                     username,
+                                                     '',
+                                                     uid,
+                                                     not userena_settings.USERENA_ACTIVATION_REQUIRED,
+                                                     userena_settings.USERENA_ACTIVATION_REQUIRED)
+
+		        myProfile = MyProfile.objects.get(user=new_user)
+            		myProfile.socialImageUrl = avatar
+            		myProfile.save()
+        	user = authenticate(username=username, password=uid)
+      		login(request, user)
+        	return HttpResponseRedirect('/')
+		
